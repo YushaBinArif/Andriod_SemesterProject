a225d4617ae8a739e741e4b6a4716a8dd0d131bf
Yush@ @rif <youshaarif13@gmail.com>
2018-01-08 22:47:14 +0500

    Made Map Activity

--- USER_END/app/src/main/java/com/example/yousha/user_end/MainActivity.java
+++ USER_END/app/src/main/java/com/example/yousha/user_end/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.yousha.user_end;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Just to test that if CallAmbulance_Activity is working//
+        startActivity(new Intent(MainActivity.this,CallAmbulance_Activity.class));
     }
 }
